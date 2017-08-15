2f8feb55312e17531850e251e2fb63ea285bca9b
rainfly123 <you@example.com>
2017-08-15 11:55:50 +0800

    ....

--- tool/hide.sh
+++ tool/hide.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 #curl -F "file=@${1}" http://live.66boss.com/upload/writev2
-curl -F "video=@${1}" "http://fuwav2.hmg66.com/api/hide?owner=100000320&detail=%E5%BA%97%E5%86%85%E6%B4%BB%E5%8A%A8&pos=%E6%B2%99%E5%8E%BF%E5%B0%8F%E5%90%83&geohash=113.3-23.20&redevpnum=2&redevptotal=0.4"
+curl -F "video=@${1}" "http://fuwav2.hmg66.com/api/hide?owner=100000320&detail=%E5%BA%97%E5%86%85%E6%B4%BB%E5%8A%A8&pos=%E6%B2%99%E5%8E%BF%E5%B0%8F%E5%90%83&geohash=113.3-23.20&redevpnum=8&redevptotal=3.4"
 
